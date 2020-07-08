8716df745557b394c6c254c0095720648e400870
Tyler McKay <tmckay1.jackson@gmail.com>
2020-07-07 22:13:12 -0400

    Added initial route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :message_board do
+    collection do
+      post :display_message
+    end
+  end
 end
