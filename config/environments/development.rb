4d9b8283722f52341131878f42b0109646a46c73
Tyler McKay <tmckay1.jackson@gmail.com>
2020-11-04 22:58:06 -0500

    fix cache store

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,14 +15,12 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
-    config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
-  else
-    config.cache_store = :null_store
   end
 
+  config.cache_store = :memory_store
   config.action_controller.perform_caching = true
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
