46c49d762042421dab626fd3dc469411afaac37f
Tyler McKay <tmckay1.jackson@gmail.com>
2020-11-04 22:38:29 -0500

    add routes

--- app/controllers/best_friend_lights_controller.rb
+++ app/controllers/best_friend_lights_controller.rb
@@ -1,10 +1,10 @@
 class BestFriendLightsController < ApplicationController
   def get_color_index
-    render json: { color_index: BestFriendLightHelper.instance.color_index }
+    render json: { color_index: BestFriendLightHelper.get_color_index() }
   end
 
   def set_color_index
-    BestFriendLightHelper.instance.color_index = params.require(:color_index)
-    render json: { color_index: BestFriendLightHelper.instance.color_index }
+    BestFriendLightHelper.set_color_index(params.require(:color_index))
+    render json: { color_index: BestFriendLightHelper.get_color_index() }
   end
 end
--- app/helpers/best_friend_light_helper.rb
+++ app/helpers/best_friend_light_helper.rb
@@ -1,5 +1,10 @@
 
-class BestFriendLightHelper
-  include Singleton
-  attr_accessor :color_index
+module BestFriendLightHelper
+  def self.get_color_index()
+    Rails.cache.fetch("color_index") { 0 }
+  end
+
+  def self.set_color_index(color_index)
+    Rails.cache.write("color_index", color_index)
+  end
 end
