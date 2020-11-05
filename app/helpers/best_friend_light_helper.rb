463ee7b7917e0131d9560696ff17ef07c0b917fb
Tyler McKay <tmckay1.jackson@gmail.com>
2020-11-04 22:44:52 -0500

    add cache

--- app/helpers/best_friend_light_helper.rb
+++ app/helpers/best_friend_light_helper.rb
@@ -1,10 +1,10 @@
 
 module BestFriendLightHelper
   def self.get_color_index()
-    Rails.cache.fetch("color_index") { 0 }
+    Rails.cache.fetch(:color_index) { 0 }
   end
 
   def self.set_color_index(color_index)
-    Rails.cache.write("color_index", color_index)
+    Rails.cache.write(:color_index, color_index)
   end
 end
