7411baeac263c8418b314653c293eb33f1b59671
eshow16 <75094946+eshow16@users.noreply.github.com>
2020-12-10 18:40:15 -0600

    edits
    i did things

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('C:/user/eshow/OneDrive/Documents/GitHub/raspberrypi_gateway/Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
